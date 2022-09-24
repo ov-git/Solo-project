585fdb25ef2efd2dd2ed1353396b4e5c809512ac
ov <virtaoskari@gmail.com>
2022-09-24 11:42:43 +0200

    for review

--- components/Main.js
+++ components/Main.js
@@ -9,10 +9,26 @@ function Main(props) {
 
   const [category, setCategory] = useState('');
   const [drinks, setDrinks] = useState([]);
+  const [search, setSearch] = useState([]);
 
   useEffect(() => {
     getByCategory();
   }, [category])
+
+  useEffect(() => {
+    handleSearch();
+
+  }, [search])
+  
+  const handleSearch = () => {
+
+    // let saved = drinks;
+
+    let filtered = drinks.filter((drink) => {
+      return drink.drinkName.includes(search);
+    })
+    setDrinks(filtered);
+  }
 
   const getByCategory = async () => {
     if (category) {
@@ -23,30 +39,20 @@ function Main(props) {
 
   const slideRight = (e) => {
     let slider = e.target.parentElement;
-
     slider.scrollLeft += window.innerWidth;
 
-    // if ((slider.scrollLeft + window.innerWidth + 50) < slider.scrollWidth) {
-    // } else {
-    //   slider.scrollLeft = 0;
-    // }
   }
 
   const slideLeft = (e) => {
     let slider = e.target.parentElement;
-
     slider.scrollLeft -= window.innerWidth;
-
-    // if ((slider.scrollLeft - window.innerWidth) >= 0) {
-    // } else {
-    //   slider.scrollLeft = slider.scrollWidth - 1;
-    // }
-
   }
 
   return (
     <div className=' h-full w-full flex-col'>
       <div className="py-4 mx-1 gap-2 pl-0 h-36 overflow-x-scroll max-w-screen flex justify-between text-white scroll-smooth">
+
+        {/* Categories */}
         <BiLeftArrow className='z-10 cursor-pointer text-black text-[50px] absolute rounded-lg left-0 bg-black px-2 h-28 w-10 hover:bg-opacity-70 hover:text-white bg-opacity-10' onClick={slideLeft}></BiLeftArrow>
         {categories.map((cat) => (
           <div className=' rounded border cursor-pointer border-black h-full shrink-0 grow w-[24vw] my-0 inline-block bg-cover bg-center overflow-hidden'
@@ -60,6 +66,12 @@ function Main(props) {
         ))}
         <BiRightArrow className='z-10 cursor-pointer text-black text-[50px] absolute rounded-lg right-0 bg-black px-2 h-28 w-10 hover:bg-opacity-70 hover:text-white bg-opacity-10' onClick={slideRight}></BiRightArrow>
       </div>
+
+      {/* Search */}
+      <form className='flex flex-col p-8 bg-red-300 w-[50vw] lg:w-[40vw] xl:w-[30vw]'>
+        <label>Search from {category} </label>
+        <input value={search} onChange={(e) => { setSearch(e.target.value) }} placeholder='Search drinks'></input>
+      </form>
 
       <div className=' m-0 p-0'>
         <Drinks drinks={drinks} setShowcase={props.setShowcase} />
