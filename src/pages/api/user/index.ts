d1a4cde29057b73f58dbe2311f7c5977570cd9d9
ov-git <virtaoskari@gmail.com>
2023-03-23 23:41:13 +0200

    deploy bug fix

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -4,22 +4,26 @@ import { getServerSession } from "next-auth";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerSession(req, res, authOptions);
-  const id = session?.user.id;
-  if (id) {
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-      },
-      include: {
-        drinks: true,
-      },
-    });
-    res.status(200);
-    res.json(user);
-    return;
-  } else {
-    res.status(200);
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const id = session?.user.id;
+    if (id) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          drinks: true,
+        },
+      });
+      res.status(200);
+      res.json(user);
+    } else {
+      throw new Error("no user found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404);
     res.json(null);
   }
 };
